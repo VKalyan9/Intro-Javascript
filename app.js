5fafb662f0560975b7106a18de8daab3c50732a0
VKalyan9 <vattemkalyan10@gmail.com>
2026-05-29 15:06:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+
+// let is a keyword used to declare variables in JavaScript. It allows you to create a variable that can be reassigned later in the code. Variables declared with let have block scope, meaning they are only accessible within the block of code where they are defined. This helps to prevent accidental changes to variables and promotes better code organization.
+console.log("Hello, World!");
+// 1 Number
+let a = 20 
+console.log(a);
+
+// 2 String
+let name = "John Doe"
+console.log(name);
+
+// 3 Boolean
+let isStudent = true
+console.log(isStudent);
+
+//4 Undefined
+let x;
+console.log(x);
+
+//5 Null
+let y = null; 
+console.log(y);
+
+// const is a constant variable that cannot be reassigned after its initial assignment. It is used to declare variables that should not change throughout the program. Once a value is assigned to a const variable, it cannot be modified or reassigned. This helps to ensure that certain values remain constant and prevents accidental changes to important data in the code.
+const pi = 3.14;
+console.log(pi);
+
+const language = "JavaScript";
+console.log(language);  
+
+console.warn("This is a warning message.");
+
+
+console.error("This is an error message.");
+
+
+
